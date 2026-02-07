9ee0e3aaa23c0950eb02ba9d10462e98e67320c1
Waruni Gunasena <121920439+WaruniGunasena@users.noreply.github.com>
2026-02-07 22:49:46 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from calculator import *
+
+
+print("Welcome to the calculator app!")
+
+print("Addition",add(2,3))
+print("Subtraction",subtract(5,2))
+
---
+++ test_calculator.py
@@ -0,0 +1,18 @@
+from calculator import add, subtract
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_add_negative():
+    assert add(-1, -1) == -2
+
+def test_subtract_negative():
+    assert subtract(-1, -1) == 0
+
+def test_add_fail():
+    assert add(2, 2) == 5  # This test is expected to fail
